1ca5412d87448b0a9eb4da534f9afb503679c767
Krunoslav Stefanek <prof.stefanek@gmail.com>
2021-01-07 14:06:38 +0000

    Adding rounding of numbers instead of converting it in numbers.

--- main.py
+++ main.py
@@ -2,18 +2,10 @@
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
 # 🚨 Don't change the code above 👆
-print(type(height))
-print(type(weight))
-bmi=float(weight)/(float(height)**2)
-print(int(bmi))
 #Write your code below this line 👇
+print(type(height));
+print(type(weight));
+bmi = float(weight) / (float(height)**2)
 
-
-
-
-
-
-
-
-
-
+print(str(weight)+"/("+str(height)+"*"+str(height)+")="+str(round(bmi,2)))
+print("\n Your BMI is:"+str(round(bmi)))
